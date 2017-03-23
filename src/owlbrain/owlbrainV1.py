14ff85e40249793190c0512e5ec40222895ffaa1
Jørgen Ljønes <jorgen.ljones@gmail.com>
2017-03-23 21:45:49 +0100

    Tidy owlbrainV1 First unit test of owlbrain

--- src/owlbrain/owlbrainV1.py
+++ src/owlbrain/owlbrainV1.py
@@ -2,26 +2,13 @@
 import unicodedata
 from datetime import datetime, timedelta
 
-# PSUDO CODE
-# EVENT: new assignment given
-# For all students taking this course:
-# Run this code and input assignment deadline
-
 def OwlbrainSheduler(assignmentDeadline, initialHoursSet, calendarEvents):
 
-    assignmentDeadline = datetime.strptime('2017-03-16 23:59:59', '%Y-%m-%d %H:%M:%S')
     remainingWorkLengthInQuarters = initialHoursSet * 4
+    calendarEvents = [unicodedata.normalize('NFKD', x).encode('ascii', 'ignore') for x in calendarEvents]
     assignedWorkSlots = []
 
-    calendarEvents = [u'2017-03-14T08:15:00EB2017-03-14T10:00:00', u'2017-03-14T10:00:00EB2017-03-14T14:00:00',
-                      u'2017-03-14T12:15:00EB2017-03-14T15:00:00', u'2017-03-14T16:15:00EB2017-03-14T18:00:00',
-                      u'2017-03-15T08:15:00EB2017-03-15T16:00:00', u'2017-03-16T08:30:00EB2017-03-16T16:00:00',
-                      u'2017-03-16T11:57:00EB2017-03-16T12:00:00', u'2017-03-16T15:15:00EB2017-03-16T18:00:00',
-                      u'2017-03-16T17:15:00EB2017-03-16T19:00:00']
-    calendarEvents = [unicodedata.normalize('NFKD', x).encode('ascii', 'ignore') for x in calendarEvents]
-    print calendarEvents
-
-    currentDayInSearch = 2         #Prior, for frist
+    currentDayInSearch = 2         #antall dager for frist forst testet for ledig tid
     currentDayInSearchCounter = 1
     currentDayInSearchInverter = 1
 
@@ -41,19 +28,6 @@ def OwlbrainSheduler(assignmentDeadline, initialHoursSet, calendarEvents):
 
             quartersCurrentDayInSearch[startQuarter:startQuarter + eventLength] = [1] * eventLength
 
-        print quartersCurrentDayInSearch
-
-            # ut ser jeg for meg at den gir : [ [event1], [event2] , [event3].... ]
-            # der event1 er for eksempel: [startid,sluttid,dato]
-            # paa formen: [08:00:00,10:00:00,2017-03-23]
-
-            # for quarters in listQuartersInADay:
-            # find index of the first quarter in a series of at least four free quarters
-                # and how many free quarters there are in the series
-
-            #assign working time in the longest series of free quarters until remainingWorkLenght = 0
-            #If there aren't enough free quarters, test the next day
-
         freeSlotsInCurrentDayInSearch = FindFreeSlots(quartersCurrentDayInSearch)[:]
 
         for freeSlot in freeSlotsInCurrentDayInSearch[0]:
@@ -65,30 +39,14 @@ def OwlbrainSheduler(assignmentDeadline, initialHoursSet, calendarEvents):
                 slotLength = remainingWorkLengthInQuarters
                 remainingWorkLengthInQuarters = 0
                 assignedWorkSlots.append(newWorkSlot(currentDateInSearch, freeSlot, slotLength))
-
-        print assignedWorkSlots
+        #TODO: pick the longest free slot in a day first
+        #TODO: put a buffer of a quarter before next events
 
         currentDayInSearch += (currentDayInSearchCounter * currentDayInSearchInverter)
         currentDayInSearchCounter += 1
         currentDayInSearchInverter *= -1
 
-        #DO stuff
-        # Sort all busy events on this day with regards to start time, stored in linked list or equivalent
-        # Traverse through list
-        #     look at start time and end time and turn all allfected bits (if not allready turned on)
-        #         Plus one quarter if the event lasts for 4 quarters or more
-    #
-    #
-    # Gaa gjennom bitstrengen fra start og finn forste ledige okt lengre enn 1 time
-    #     Hvis den finnes
-    #         Lag en arbeidsokt i den ledige tiden, maks saa mange timer som gjenstaar
-    #     Hvis den ikke finnes
-    #         Dag = Dag + inverter*counter
-    #         counter++
-    #         inverter = -inverter
-    #         Gaa til Dag
-
-    return()
+    return assignedWorkSlots
 
 def FindFreeSlots(list):
     freeQuarters = []
@@ -115,8 +73,6 @@ def FindFreeSlots(list):
     for lengths in slotLengths[:-1]:
         startFreeQuarters.append(freeQuarters[lengths])
 
-    print [startFreeQuarters]+[slotLengths]
-
     return [startFreeQuarters]+[slotLengths]
 
 def newWorkSlot(currentDateInSearch, freeSlot, slotLength):
@@ -126,7 +82,3 @@ def newWorkSlot(currentDateInSearch, freeSlot, slotLength):
               timedelta(0, slotLength * 15 * 60)
     newWorkSlot = [startTime.strftime('%H:%M:%S'), endTime.strftime('%H:%M:%S'), currentDateInSearch]
     return newWorkSlot
-
-
-if __name__ == '__main__':
-    OwlbrainSheduler(0, 3, 0)
